6564f859fd66dc1bb57c858e4db7f665e16a72da
Cristhian-Medina <criscis.mecasa@gmail.com>
2022-08-12 21:59:55 -0500

    feat: add initial styles within globals and variables, add @styles path in jsconfig

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Custom404() {
   return (
     <>
-      <h1>404 -  </h1>
+      <h1>404</h1>
       <p>Esta Pagina esta fuera de servicio</p>
       <Link href="/">
         <a>Volver al Inicio</a>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,8 @@
 import Layout from '@components/Layout'
 
+import '@styles/globals.css'
+import '@styles/variables.css'
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
