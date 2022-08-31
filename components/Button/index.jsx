23254c0f69626f132d3dce1a38d8a6682e676b0a
Cristhian-Medina <criscis.mecasa@gmail.com>
2022-08-31 01:09:30 -0500

    refactor: add button type link in Button component

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -1,6 +1,60 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Button = ({className, type, value, disabled}) => {
+const Button = ({className, type, value, href, disabled}) => {
+  if(type === 'link') {
+    return (
+      <>
+        <Link href={href}>
+          <a className={className}>{value}</a>
+        </Link>
+
+        <style jsx>
+          {`
+            a {
+              display: block;
+              border: none;
+              outline: none;
+              text-transform: uppercase;
+              font: var(--f-button);
+              letter-spacing: 2px;
+              padding: 16px 0;
+              text-align: center;
+              transition: background-color .3s;
+            }
+
+            .primary {
+              background-color: var(--color-primary-dark);
+              color: var(--color-secondary-light);
+              width: 200px;
+            }
+
+            .primary:hover {
+              background-color: var(--color-primary-light);
+            }
+
+            .secondary {
+              background-color: var(--color-secondary-light);
+              color: var(--color-primary-dark);
+              border: 1px solid var(--color-primary-dark);
+              width: 162px;
+            }
+
+            .secondary:hover {
+              background-color: var(--color-primary-dark);
+              color: var(--color-secondary-light);
+            }
+
+            .secondary::disabled {
+              background-color: var(--color-secondary-gray);
+              color: var(--color-secondary-gray);
+              border: 1px solid var(--color-secondary-gray);
+            }
+          `}
+        </style>
+      </>
+    )
+  }
 
   return (
     <>
@@ -17,6 +71,8 @@ const Button = ({className, type, value, disabled}) => {
             border: none;
             outline: none;
             text-transform: uppercase;
+            font: var(--f-button);
+            letter-spacing: 2px;
             padding: 16px 0;
             transition: background-color .3s;
           }
@@ -51,12 +107,6 @@ const Button = ({className, type, value, disabled}) => {
             background-color: var(--color-secondary-gray);
             color: var(--color-secondary-gray);
             border: 1px solid var(--color-secondary-gray);
-          }
-
-          @media screen and (max-width: 900px) {
-          }
-
-          @media screen and (max-width: 500px) {
           }
         `}
       </style>
